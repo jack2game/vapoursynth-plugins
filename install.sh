b5ce46772ab230a991f11a9e5c46c6f2c4a49f89
jack2game <rex.txt@gmail.com>
2021-11-09 14:52:59 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,8 +4,8 @@ cd /usr/src/ffms2
 ./configure
 make
 make install
-rm -rfv /usr/src/ffms2
 cd /usr/src
+rm -rfv /usr/src/ffms2
 ln -s /usr/local/lib/libffms2.so /usr/local/lib/vapoursynth/libffms2.so
 
 git clone https://github.com/l-smash/l-smash.git /usr/src/lsmas
@@ -13,16 +13,24 @@ cd /usr/src/lsmas
 ./configure
 make lib
 make install-lib
-rm -rfv /usr/src/lsmas
 cd /usr/src
+rm -rfv /usr/src/lsmas
 
 git clone https://github.com/AkarinVS/L-SMASH-Works.git /usr/src/lsmas
 cd /usr/src/lsmas/VapourSynth
 meson build
 ninja -C build
-rm -rfv /usr/src/lsmas
 cd /usr/src
+rm -rfv /usr/src/lsmas
 
 pip3 install git+https://git.concertos.live/AHD/awsmfunc.git
 
 pip3 install numpy
+
+#git clone https://code.videolan.org/videolan/x264/ /usr/src/x264
+#cd /usr/src/x264
+#./configure
+#make
+#make install
+#cd /usr/src
+#rm -rfv /usr/src/lsmas
