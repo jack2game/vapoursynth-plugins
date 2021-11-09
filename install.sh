9d72f0f80416173c0cf30d37027825e1934b0b07
jack2game <rex.txt@gmail.com>
2021-11-09 14:14:57 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,19 +1,3 @@
-git clone https://github.com/sekrit-twc/zimg.git /usr/src/zimg
-cd /usr/src/zimg
-./autogen.sh
-./configure
-make
-make install 
-rm -rfv /usr/src/zimg
-
-git clone -b R54 https://github.com/vapoursynth/vapoursynth.git /usr/src/vapoursynth
-cd /usr/src/vapoursynth
-./autogen.sh
-./configure
-make
-make install
-rm -rfv /usr/src/vapoursynth
-
 git clone https://github.com/FFMS/ffms2.git /usr/src/ffms2
 cd /usr/src/ffms2
 ./autogen.sh
