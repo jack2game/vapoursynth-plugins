f186d0c11b5583132313e22198520864049a9d1d
jack2game <rex.txt@gmail.com>
2021-11-09 17:11:53 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -25,8 +25,44 @@ cd /usr/src
 cp /usr/src/lsmasvs/VapourSynth/build/libvslsmashsource.so /usr/local/lib/vapoursynth/
 rm -rfv /usr/src/lsmasvs
 
+git clone https://github.com/Irrational-Encoding-Wizardry/Vapoursynth-RemapFrames.git /usr/src/remap
+cd /usr/src/remap
+meson build
+ninja -C build
+cd /usr/src
+cp /usr/src/remap/build/libremapframes.so /usr/local/lib/vapoursynth/
+rm -rfv /usr/src/remap
+
+git clone https://github.com/dubhater/vapoursynth-fillborders.git /usr/src/fillborders
+cd /usr/src/fillborders
+meson build
+ninja -C build
+cd /usr/src
+cp /usr/src/fillborders/build/libfillborders.so /usr/local/lib/vapoursynth/
+rm -rfv /usr/src/fillborders
+
+git clone https://gitlab.com/Ututu/VS-ContinuityFixer.git /usr/src/cf
+cd /usr/src/cf
+g++ -c -std=gnu++11 -fPIC -I. -o continuity.o continuity.cpp
+g++ -shared -fPIC -o libcf.so continuity.o
+cd /usr/src
+cp /usr/src/cf/libcf.so /usr/local/lib/vapoursynth/
+rm -rfv /usr/src/cf
+
+#git clone https://github.com/HomeOfAviSynthPlusEvolution/neo_f3kdb.git /usr/src/neo_f3kdb
+#cd /usr/src/neo_f3kdb
+
+
 pip3 install git+https://git.concertos.live/AHD/awsmfunc.git
+pip3 install git+https://github.com/Irrational-Encoding-Wizardry/vsutil.git
 pip3 install numpy
+wget -P /python_module https://github.com/HomeOfVapourSynthEvolution/havsfunc/raw/master/havsfunc.py
+wget -P /python_module https://github.com/Irrational-Encoding-Wizardry/fvsfunc/raw/master/fvsfunc.py
+wget -P /python_module https://github.com/Irrational-Encoding-Wizardry/kagefunc/raw/master/kagefunc.py
+wget -P /python_module https://gitlab.com/Ututu/adptvgrnmod/-/raw/master/adptvgrnMod.py
+wget -P /python_module https://github.com/jack2game/solarcurve/raw/main/solar.py
+wget -P /python_module https://github.com/dubhater/vapoursynth-adjust/raw/master/adjust.py
+wget -P /python_module https://github.com/HomeOfVapourSynthEvolution/mvsfunc/raw/master/mvsfunc.py
 
 #git clone https://code.videolan.org/videolan/x264/ /usr/src/x264
 #cd /usr/src/x264
